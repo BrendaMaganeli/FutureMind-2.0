8ddd94eec6eac61a331f65ce93cf6c59cf942550
MATEUS DA SILVA <mateus_silva44@estudante.sesisenai.org.br>
2025-02-18 14:09:20 -0300

    novas alterações na tela de agendamento

--- front/src/pages/TelaAgendamento.jsx
+++ front/src/pages/TelaAgendamento.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './CSS/TelaAgendamento.css'
-
+import logoHorizontal from '../assets/logoHorizontal.svg'
 const TelaAgendamento = () => {
 
   return (
@@ -11,17 +11,17 @@ const TelaAgendamento = () => {
           <img className='foto_perfil' src="user-profile-circle.svg" alt="" />
           <div className='container-perfil_informações'>
             <div className='div_nome_profissional'>
-             <h1 className='nome_profissional'>nome usuario</h1>
+             <h1 className='nome_profissional'>Nome usuario</h1>
             </div>
             <div className='div-CRC'>
               <p className='CRC'>CRC:111111/09</p>
             </div>
             <div className='div_button_chat'>
-              <button className='button_chat'>abrir chat</button>
+              <button className='button_chat'>Abrir chat</button>
             </div>
           </div>
           </div>
-            <img className='logo' src="user-profile-circle.svg" alt="" />
+            <img className='logo' src={logoHorizontal} alt="" />
         </div>
       </div>
       <div className='container_tela_agendamento_direita'>

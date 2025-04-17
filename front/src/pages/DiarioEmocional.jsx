c284b5bc869c57c12d34672352734d1674c36774
BrendaMaganeli <brenda_casimiro@estudante.sesisenai.org.br>
2025-04-17 14:27:35 -0300

    diario emocional atualizado

--- front/src/pages/DiarioEmocional.jsx
+++ front/src/pages/DiarioEmocional.jsx
@@ -81,13 +81,8 @@ function DiarioEmocional() {
   }, [notas, notaAtiva]);
 
   return (
-    <motion.div
-      className={`container-diarioemocional ${telaCheia ? 'fullscreen' : ''}`}
-      initial={{ opacity: 0, scale: 0.9 }}
-      animate={{ opacity: 1, scale: 1 }}
-      exit={{ opacity: 0, scale: 0.9 }}
-      transition={{ type: 'spring', stiffness: 100, damping: 30 }}
-    >
+<div className={`container-diarioemocional ${telaCheia ? 'fullscreen' : ''}`}>
+
       <div className='lado-esquerdoDiario'>
         <div className='div-cabecalhodiario'>
           <div className='cacecalhomenor'>
@@ -203,7 +198,7 @@ function DiarioEmocional() {
           </div>
         </div>
       )}
-    </motion.div>
+      </div>
   );
 }
 

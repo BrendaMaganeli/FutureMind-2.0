d827740fa1d13771ed706badf1e0c033fc2b8249
VitorMatt <vitor.azevedoo2812@gmail.com>
2025-06-12 19:04:59 -0300

    Video Conferencia

--- front/src/pages/Call2.jsx
+++ front/src/pages/Call2.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import "./CSS/Call.css";
 
-const socket = io('http://futuremind-20-production.up.railway.app', {
+const socket = io('https://futuremind-20-production.up.railway.app', {
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
   timeout: 10000
